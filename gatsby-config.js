199a94bd55ccb45d8c1f66ec2f452b5af2fd00dd
yuki goto <goto.ipod5@gmail.com>
2021-05-15 13:54:55 +0900

    アイコン画像追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,18 @@ module.exports = {
             purpose: "any maskable",
           },
           {
+            src: "icons/icon-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
             src: "icons/icon-144x144.png",
             sizes: "144x144",
             type: "image/png",
