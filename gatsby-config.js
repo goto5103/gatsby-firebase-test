160862d4dd307e62e82ecb232415400df5e9f206
yuki goto <goto.ipod5@gmail.com>
2021-05-15 17:26:43 +0900

    アイコン画像の指定を一旦変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,72 +43,73 @@ module.exports = {
         theme_color: `#7b68ee`,
         display: `standalone`,
         legacy: false, 
-        icons: [
-          {
-            src: "icons/icon-48x48.png",
-            sizes: "48x48",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-64x64.png",
-            sizes: "64x64",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-72x72.png",
-            sizes: "72x72",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-128x128.png",
-            sizes: "128x128",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-180x180.png",
-            sizes: "180x180",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-384x384.png",
-            sizes: "384x384",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          }
-        ]
-        // icon:"./src/images/g139.png",
-        // icon_options: {
-        //   purpose: `maskable`,
-        // },
+        icon:"icons/icon-512x512.png",
+        icon_options: {
+          purpose: `maskable`,
+        },
+        // icons: [
+        //   {
+        //     src: "icons/icon-48x48.png",
+        //     sizes: "48x48",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-64x64.png",
+        //     sizes: "64x64",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-72x72.png",
+        //     sizes: "72x72",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-128x128.png",
+        //     sizes: "128x128",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-144x144.png",
+        //     sizes: "144x144",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-180x180.png",
+        //     sizes: "180x180",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-256x256.png",
+        //     sizes: "256x256",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-384x384.png",
+        //     sizes: "384x384",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "icons/icon-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //     purpose: "any maskable",
+        //   }
+        // ]
+
       },
     },
     `gatsby-plugin-offline`,
